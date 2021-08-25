4b54c5f858aa65ad65643a27561032904a246adf
Irene <iruuunechka@gmail.com>
2021-08-25 23:59:53 +0300

    Fix unhashable type: 'list' error

--- gadma/hqea/greedy_q_agent.py
+++ gadma/hqea/greedy_q_agent.py
@@ -1,4 +1,3 @@
-import operator
 import numpy as np
 
 
@@ -16,26 +15,26 @@ class GreedyQAgent:
         return self.strict
 
     def update_experience(self, cur_state, new_state, action, reward):
-        old = self.q_map.get([cur_state, action], 0.0)
+        old = self.q_map.get((cur_state, action), 0.0)
         self.q_map[cur_state, action] = old + self.alpha * (reward + self.gamma * self.max_in_q(new_state) - old)
 
     def choose_action(self, state):
         if np.random.random() < self.epsilon:
             return np.random.choice(range(self.actions))
-        if self.q_map.get([state, 0], 0.0) == self.q_map.get([state, 1], 0.0):
+        if self.q_map.get((state, 0), 0.0) == self.q_map.get((state, 1), 0.0):
             return -1
         return self.argmax_in_q(state)
 
     def max_in_q(self, state):
         maximum = -1
         for i in range(self.actions):
-            maximum = max(maximum, self.q_map.get([state, i], 0.0))
+            maximum = max(maximum, self.q_map.get((state, i), 0.0))
         return maximum
 
     def argmax_in_q(self, state):
-        if self.q_map.get([state, 0], 0) == self.q_map.get([state, 1], 0):
+        if self.q_map.get((state, 0), 0) == self.q_map.get((state, 1), 0):
             return np.random.choice([0, 1])
-        if self.q_map.get([state, 0], 0) > self.q_map.get([state, 1], 0):
+        if self.q_map.get((state, 0), 0) > self.q_map.get((state, 1), 0):
             return 0
         else:
             return 1
