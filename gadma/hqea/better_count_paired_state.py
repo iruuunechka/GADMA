95510b3793b7807c5542eefee78488fe69932e81
Irene <iruuunechka@gmail.com>
2021-09-23 19:47:20 +0300

    Fixed new state

--- gadma/hqea/better_count_paired_state.py
+++ gadma/hqea/better_count_paired_state.py
@@ -1,4 +1,4 @@
 class BetterCountPairedState:
     @staticmethod
     def calculate(better_mutants, better_than_best_mutants):
-        return better_mutants + " " + better_than_best_mutants
+        return str(better_mutants) + " " + str(better_than_best_mutants)
