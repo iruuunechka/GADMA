a5ab2363a63b7b14209aec236cae1c009c462ebc
Irene <iruuunechka@gmail.com>
2021-09-23 19:05:27 +0300

    Testing when reward depends mostly on the first result

--- gadma/hqea/div_reward.py
+++ gadma/hqea/div_reward.py
@@ -1,3 +1,3 @@
 class DivReward:
     def calculate(self, new_fitness, old_fitness, coef):
-        return (1.0 - float(new_fitness + 1) / (old_fitness + 1)) / coef
+        return (1.0 - float(new_fitness + 1) / (old_fitness + 1)) / (coef ** 6)
